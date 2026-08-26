fab7fe0d3a417474f08c75038c2876c4973cb9cb
Aleina Rossui <admin@promoflow.net>
2026-08-26 02:44:04 -0400

    “…”

--- pages/general/charging/charging.js
+++ pages/general/charging/charging.js
@@ -1,18 +1,16 @@
 class Charging {
   constructor() {
-    this.hideShowcharging(true);
-    // Ocultar después de 3 segundos
-    setTimeout(() => {
-      this.hideShowcharging(false);
-    }, 1500);
+    this.element = document.querySelector('.charging_background');
+    this.hideShowcharging(false);
   }
 
   hideShowcharging(action) {
-    const charging_background = document.querySelector('.charging_background');
-    if (charging_background) {
-      charging_background.style.display = action ? "flex" : "none";
+    if (this.element) {
+      this.element.style.display = action ? "flex" : "none";
+      this.element.setAttribute('aria-hidden', action ? 'false' : 'true');
     }
   }
 }
 
 const chargingClass = new Charging();
+window.chargingClass = chargingClass;
